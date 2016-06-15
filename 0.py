ec2025f3eb118f32a5fdd85ba2f98a84c641850f
theidentity <balagopal.u@gmail.com>
2016-06-15 23:11:16 +0530

    Basic input output

--- 0.py
+++ 0.py
@@ -1,2 +1,7 @@
+# Basic Input and output
+
+# var = raw_input("message") allows input with prompt
+# print or print() allows output to terminal
+
 name = raw_input("Please tell me your name : ")
 print 'Hello '+name
