72f5d94df45fd26841cb351226428c116b55dbd5
theidentity <balagopal.u@gmail.com>
2016-06-15 23:14:43 +0530

    Command line argument

--- 1.py
+++ 1.py
@@ -1,3 +1,9 @@
+# Taking command line arguments for input
+
+# Python allows for intuitive combination of operators.
+# 2*Hi is HiHi
+# sys.argv[1] takes the first argument from the command line
+
 import sys
 print "Hello "+sys.argv[1]
 print 3*"Bye"
